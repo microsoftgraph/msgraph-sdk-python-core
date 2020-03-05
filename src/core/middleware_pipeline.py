261f972c7a578aa965cc3f257d04178e8cd1bcca
jobala <japhethobalak@gmail.com>
2020-03-05 14:26:59 +0300

    Create a middleware pipeline

---
+++ src/core/middleware_pipeline.py
@@ -0,0 +1,16 @@
+from requests.adapters import HTTPAdapter
+
+
+class MiddlewarePipeline(HTTPAdapter):
+    def __init__(self):
+        super(MiddlewarePipeline, self).__init__(self)
+        self._head = None
+
+    def add_middleware(self, middleware):
+        if self._head is None:
+            self._head = middleware
+        self._head._next = middleware
+
+    def send(self, request, **kwargs):
+        if self._head is not None:
+            return self._head.send(request, **kwargs)

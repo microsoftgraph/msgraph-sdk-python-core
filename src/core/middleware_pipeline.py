eb4ab8ff7df5fdc963f1e61e8d92496306ae069b
jobala <japhethobalak@gmail.com>
2020-03-07 18:44:53 +0300

    Refactor middleware pipeline

--- src/core/middleware_pipeline.py
+++ src/core/middleware_pipeline.py
@@ -7,22 +7,21 @@ from urllib3 import PoolManager
 class MiddlewarePipeline(HTTPAdapter):
     def __init__(self):
         super().__init__()
-        self._head = None
+        self._middleware = None
+        self.poolmanager = PoolManager(ssl_version=ssl.PROTOCOL_TLSv1)
 
     def add_middleware(self, middleware):
-        if self._head is None:
-            self._head = middleware
-            return
-
-        self._head.next = middleware
+        if self._middleware_present():
+            self._middleware.next = middleware
+        else:
+            self._middleware = middleware
 
     def send(self, request, **kwargs):
-        if self._head is not None:
-            return self._head.send(request, **kwargs)
+        if self._middleware_present():
+            return self._middleware.send(request, **kwargs)
+        # No middleware in pipeline, call superclass' send
         return super().send(request, **kwargs)
 
-    def init_poolmanager(self, connections, maxsize, block=False, **pool_kwargs):
-        self.poolmanager = PoolManager(num_pools=connections,
-                                       maxsize=maxsize,
-                                       block=block,
-                                       ssl_version=ssl.PROTOCOL_TLSv1)
+    def _middleware_present(self):
+        return self._middleware
+

63878d5d6657a4fbed7153f0f50efb5bf9b75322
jobala <japhethobalak@gmail.com>
2020-03-26 18:43:14 +0300

    Add return statement

--- src/middleware/options/middleware_control.py
+++ src/middleware/options/middleware_control.py
@@ -3,7 +3,7 @@ class MiddlewareControl:
         self.middleware_options = {}
 
     def set(self, middleware_option_name, middleware_option):
-        self.middleware_options.update({middleware_option_name, middleware_option})
+        self.middleware_options.update({middleware_option_name: middleware_option})
 
     def get(self, middleware_option_name):
-        self.middleware_options.get(middleware_option_name)
+        return self.middleware_options.get(middleware_option_name)

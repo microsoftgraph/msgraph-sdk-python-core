105ef5e338aacce3e4627eb184e36e512e872c67
jobala <japhethobalak@gmail.com>
2020-03-30 10:44:54 +0300

    Remove accessor methods

--- src/middleware/authorization_provider.py
+++ src/middleware/authorization_provider.py
@@ -2,17 +2,9 @@ from ._base_auth import AuthProviderBase, TokenCredential
 
 
 class TokenCredentialAuthProvider(AuthProviderBase):
-    def __init__(self, credential: TokenCredential, scopes=''):
+    def __init__(self, credential: TokenCredential):
         self.credential = credential
-        self.scopes = scopes
-
-    @property
-    def scopes(self):
-        return self.scopes
-
-    @scopes.setter
-    def scopes(self, scopes):
-        self.scopes = scopes
+        self.scopes = ''
 
     def get_access_token(self):
         return self.credential.get_token(self.scopes)[0]

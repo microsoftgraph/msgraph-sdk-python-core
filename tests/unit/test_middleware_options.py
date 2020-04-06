2050aeaa280219769537bdb3b9b5ccae73c14307
jobala <japhethobalak@gmail.com>
2020-04-07 01:21:27 +0300

    Remove unnecessary new line

--- tests/unit/test_middleware_options.py
+++ tests/unit/test_middleware_options.py
@@ -8,4 +8,3 @@ class TestMiddlewareOptions(TestCase):
         graph_scopes = 'https://graph.microsoft.com/v1.0?scopes=mail.read%20user.read%20'
         auth_options = AuthMiddlewareOptions(scopes=['mail.read', 'user.read'])
         self.assertEqual(auth_options.scopes, graph_scopes)
-

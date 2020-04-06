e66c10d826dd203a3e6c005f11a7820798aeb856
jobala <japhethobalak@gmail.com>
2020-04-07 00:20:19 +0300

    Support multiple scopes through Authorization provider

--- src/middleware/options/auth_middleware_options.py
+++ src/middleware/options/auth_middleware_options.py
@@ -1,3 +1,20 @@
+from src.constants import BASE_URL
+
+
 class AuthMiddlewareOptions:
     def __init__(self, scopes=''):
         self.scopes = scopes
+
+    @property
+    def scopes(self):
+        return self._scopes
+
+    @scopes.setter
+    def scopes(self, list_of_scopes):
+        graph_scopes = BASE_URL + '?scopes='
+
+        for scope in list_of_scopes:
+            graph_scopes += scope + '%20'
+
+        self._scopes = graph_scopes
+

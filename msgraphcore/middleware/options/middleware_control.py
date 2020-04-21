342b15a861fb65b704ace8ea49391b7133af62d0
jobala <japhethobalak@gmail.com>
2020-04-21 10:27:26 +0300

    Add _get_middleware_options decorator to middleware_control.py

--- msgraphcore/middleware/options/middleware_control.py
+++ msgraphcore/middleware/options/middleware_control.py
@@ -1,3 +1,7 @@
+from msgraphcore.constants import AUTH_MIDDLEWARE_OPTIONS
+from ..options.auth_middleware_options import AuthMiddlewareOptions
+
+
 class MiddlewareControl:
     def __init__(self):
         self.middleware_options = {}
@@ -7,3 +11,18 @@ class MiddlewareControl:
 
     def get(self, middleware_option_name):
         return self.middleware_options.get(middleware_option_name, None)
+
+    def get_middleware_options(self, func):
+        self._reset_middleware_options()
+
+        def wrapper(*args, **kwargs):
+            scopes = kwargs.pop('scopes')
+            self.set(AUTH_MIDDLEWARE_OPTIONS, AuthMiddlewareOptions(scopes))
+            return func(*args, **kwargs)
+        return wrapper
+
+    def _reset_middleware_options(self):
+        self.middleware_options = {}
+
+
+middleware_control = MiddlewareControl()

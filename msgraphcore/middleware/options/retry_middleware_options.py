2e0f9ce648c539f8a90e2ad8aedd0eb1bcab7010
samwelkanda <samwelkanda@gmail.com>
2021-02-26 06:22:22 +0300

    Test retry middleware options

--- msgraphcore/middleware/options/retry_middleware_options.py
+++ msgraphcore/middleware/options/retry_middleware_options.py
@@ -3,6 +3,5 @@ class RetryMiddlewareOptions:
         self.retry_total: int = retry_configs.get('retry_total')
         self.retry_backoff_factor: int = retry_configs.get('retry_backoff_factor')
         self.retry_backoff_max: int = retry_configs.get('retry_backoff_max')
-        self.retry_mode: str = retry_configs.get('retry_mode')
         self.timeout: int = retry_configs.get('timeout')
         self.retry_on_status_codes: [int] = retry_configs.get('retry_on_status_codes')
--- tests/unit/test_middleware_options.py
+++ tests/unit/test_middleware_options.py
@@ -1,10 +1,30 @@
 from unittest import TestCase
 
 from msgraphcore.middleware.options.auth_middleware_options import AuthMiddlewareOptions
+from msgraphcore.middleware.options.retry_middleware_options import RetryMiddlewareOptions
+
+retry_settings = {
+    'retry_total': 5,
+    'retry_backoff_factor': 0.8,
+    'retry_backoff_max': 100,
+    'timeout': 100,
+    'retry_on_status_codes': [502, 503],
+}
 
 
-class TestMiddlewareOptions(TestCase):
+class TestAuthMiddlewareOptions(TestCase):
     def test_multiple_scopes(self):
         graph_scopes = 'https://graph.microsoft.com/v1.0?scopes=mail.read%20user.read%20'
         auth_options = AuthMiddlewareOptions(graph_scopes)
         self.assertEqual(auth_options.scopes, graph_scopes)
+
+
+def test_retry_middleware_options():
+
+    retry_options = RetryMiddlewareOptions(retry_settings)
+
+    assert retry_settings["retry_total"] == retry_options.retry_total
+    assert retry_settings["retry_backoff_factor"] == retry_options.retry_backoff_factor
+    assert retry_settings["retry_backoff_max"] == retry_options.retry_backoff_max
+    assert retry_settings["timeout"] == retry_options.timeout
+    assert retry_settings["retry_on_status_codes"] == retry_options.retry_on_status_codes

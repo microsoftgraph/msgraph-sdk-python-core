ec82263c35f74fac8e086a295de66d3b3941f4d6
samwelkanda <samwelkanda@gmail.com>
2021-03-03 21:00:27 +0300

    Add tests for the retry handler

--- tests/unit/test_retry_handler.py
+++ tests/unit/test_retry_handler.py
@@ -1,6 +1,8 @@
 import pytest
+import requests
 import responses
 
+from msgraphcore.constants import BASE_URL
 from msgraphcore.graph_session import GraphSession
 from msgraphcore.middleware.retry_middleware import RetryMiddleware
 
@@ -98,3 +100,59 @@ def test_configure_settings_with_session_configs():
     assert settings['timeout'] == retry_handler.DEFAULT_RETRY_TIME_LIMIT
     assert settings['retry_codes'] == retry_handler._retry_on_status_codes
     assert settings['methods'] == retry_handler._allowed_methods
+
+
+@responses.activate
+def test_method_retryable_with_valid_method():
+    """
+    Test the is_method_retryable helper method with a valid HTTP response code
+    """
+    responses.add(responses.GET, BASE_URL, status=502)
+    response = requests.get(BASE_URL)
+
+    retry_handler = RetryMiddleware(retry_configs={})
+    settings = retry_handler.configure_retry_settings()
+
+    assert retry_handler._is_method_retryable(settings, response.request)
+
+
+@responses.activate
+def test_method_retryable_with_invalid_method():
+    """
+    Test the is_method_retryable helper method with an invalid HTTP response code
+    """
+    responses.add(responses.POST, BASE_URL, body="Test", status=503)
+    response = requests.post(BASE_URL, data={"left": 1, "right": 3})
+
+    retry_handler = RetryMiddleware(retry_configs={})
+    settings = retry_handler.configure_retry_settings()
+
+    assert not retry_handler._is_method_retryable(settings, response.request)
+
+
+@responses.activate
+def test_should_retry_valid():
+    """
+    Test the should_retry method with a valid HTTP method and response code
+    """
+    responses.add(responses.GET, BASE_URL, status=503)
+    response = requests.get(BASE_URL)
+
+    retry_handler = RetryMiddleware(retry_configs={})
+    settings = retry_handler.configure_retry_settings()
+
+    assert retry_handler.should_retry(settings, response)
+
+
+@responses.activate
+def test_should_retry_invalid():
+    """
+    Test the should_retry method with an valid HTTP response code
+    """
+    responses.add(responses.GET, BASE_URL, status=502)
+    response = requests.get(BASE_URL)
+
+    retry_handler = RetryMiddleware(retry_configs={})
+    settings = retry_handler.configure_retry_settings()
+
+    assert not retry_handler.should_retry(settings, response)

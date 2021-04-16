d07932ade37ea0bc2e157f5fd6bbebf7dfabe5b2
samwelkanda <samwelkanda@gmail.com>
2021-04-16 15:12:02 +0300

    Replace GraphSession with GraphClient

--- msgraphcore/graph_client.py
+++ msgraphcore/graph_client.py
@@ -1,51 +1,37 @@
-"""
-Graph Session
-"""
+from typing import Optional
+
 from requests import Session
 
-from msgraphcore.constants import BASE_URL, SDK_VERSION
+from msgraphcore.client_factory import HTTPClientFactory
 from msgraphcore.middleware.abc_token_credential import TokenCredential
-from msgraphcore.middleware.authorization import AuthorizationHandler
-from msgraphcore.middleware.middleware import BaseMiddleware, MiddlewarePipeline
+from msgraphcore.middleware.middleware import BaseMiddleware
 from msgraphcore.middleware.options.middleware_control import middleware_control
 
 
-class GraphSession(Session):
-    """Extends Session with Graph functionality
-
-    Extends Session by adding support for middleware options and middleware pipeline
-    """
+class GraphClient:
+    """Constructs a custom HTTPClient to be used for requests against Microsoft Graph"""
     def __init__(
-        self,
-        credential: TokenCredential,
-        scopes: [str] = ['.default'],
-        middleware: list = [],
-        api_version: str = 'v1.0'
+        self, credential: Optional[TokenCredential], session: Optional[Session],
+        middleware: Optional[[BaseMiddleware]], **kwargs
     ):
-        super().__init__()
-        self._append_sdk_version()
-        self._base_url = BASE_URL + '/' + api_version
-
-        auth_handler = AuthorizationHandler(credential, scopes)
-
-        # The authorization handler should be the first middleware in the pipeline.
-        middleware.insert(0, auth_handler)
-        self._register(middleware)
+        """
+        Class constructor that accepts a session object and kwargs to
+        be passed to the HTTPClientFactory
+        """
+        self.graph_session = get_graph_session(credential, session, middleware, **kwargs)
 
     @middleware_control.get_middleware_options
     def get(self, url: str, **kwargs):
         r"""Sends a GET request. Returns :class:`Response` object.
-
         :param url: URL for the new :class:`Request` object.
         :param \*\*kwargs: Optional arguments that ``request`` takes.
         :rtype: requests.Response
         """
-        return super().get(self._graph_url(url), **kwargs)
+        return self.graph_session.get(self._graph_url(url), **kwargs)
 
     @middleware_control.get_middleware_options
     def post(self, url, data=None, json=None, **kwargs):
         r"""Sends a POST request. Returns :class:`Response` object.
-
         :param url: URL for the new :class:`Request` object.
         :param data: (optional) Dictionary, list of tuples, bytes, or file-like
             object to send in the body of the :class:`Request`.
@@ -53,72 +39,66 @@ class GraphSession(Session):
         :param \*\*kwargs: Optional arguments that ``request`` takes.
         :rtype: requests.Response
         """
-        return super().post(self._graph_url(url), data, json, **kwargs)
+        return self.graph_session.post(self._graph_url(url), data, json, **kwargs)
 
     @middleware_control.get_middleware_options
     def put(self, url, data=None, **kwargs):
         r"""Sends a PUT request. Returns :class:`Response` object.
-
         :param url: URL for the new :class:`Request` object.
         :param data: (optional) Dictionary, list of tuples, bytes, or file-like
             object to send in the body of the :class:`Request`.
         :param \*\*kwargs: Optional arguments that ``request`` takes.
         :rtype: requests.Response
         """
-        return super().put(self._graph_url(url), data, **kwargs)
+        return self.graph_session.put(self._graph_url(url), data, **kwargs)
 
     @middleware_control.get_middleware_options
     def patch(self, url, data=None, **kwargs):
         r"""Sends a PATCH request. Returns :class:`Response` object.
-
         :param url: URL for the new :class:`Request` object.
         :param data: (optional) Dictionary, list of tuples, bytes, or file-like
             object to send in the body of the :class:`Request`.
         :param \*\*kwargs: Optional arguments that ``request`` takes.
         :rtype: requests.Response
         """
-        return super().patch(self._graph_url(url), data, **kwargs)
+        return self.graph_session.patch(self._graph_url(url), data, **kwargs)
 
     @middleware_control.get_middleware_options
     def delete(self, url, **kwargs):
         r"""Sends a DELETE request. Returns :class:`Response` object.
-
         :param url: URL for the new :class:`Request` object.
         :param \*\*kwargs: Optional arguments that ``request`` takes.
         :rtype: requests.Response
         """
-        return super().delete(self._graph_url(url), **kwargs)
+        return self.graph_session.delete(self._graph_url(url), **kwargs)
 
     def _graph_url(self, url: str) -> str:
         """Appends BASE_URL to user provided path
-
         :param url: user provided path
         :return: graph_url
         """
-        return self._base_url + url if (url[0] == '/') else url
+        return self.graph_session.base_url + url if (url[0] == '/') else url
 
-    def _register(self, middleware: [BaseMiddleware]) -> None:
-        """Adds middleware to middleware_pipeline
 
-        :param middleware: list of middleware
-        """
-        if middleware:
-            middleware_pipeline = MiddlewarePipeline()
+_INSTANCE = None
 
-            for ware in middleware:
-                middleware_pipeline.add_middleware(ware)
 
-            self.mount('https://', middleware_pipeline)
+def get_graph_session(
+    credential: Optional[TokenCredential], session: Optional[Session],
+    middleware: Optional[[BaseMiddleware]], **kwargs
+):
+    """Method to always return a single instance of a HTTP Client"""
+    global _INSTANCE
 
-    def _append_sdk_version(self) -> None:
-        """Updates sdkVersion in headers with comma-separated new values
-        """
-        if 'sdkVersion' in self.headers:
-            self.headers.update(
-                {
-                    'sdkVersion':
-                    'graph-python-' + SDK_VERSION + ', ' + self.headers.get('sdkVersion')
-                }
-            )
-        else:
-            self.headers.update({'sdkVersion': 'graph-python-' + SDK_VERSION})
+    if credential and middleware:
+        raise Exception("Invalid parameters! Both TokenCredential and middleware cannot be passed")
+    if not credential and not middleware:
+        raise ValueError("Invalid parameters!. Missing TokenCredential or middleware")
+    if _INSTANCE is None:
+        if credential:
+            _INSTANCE = HTTPClientFactory(session,
+                                          **kwargs).create_with_default_middleware(credential)
+        elif middleware:
+            _INSTANCE = HTTPClientFactory(session,
+                                          **kwargs).create_with_custom_middleware(middleware)
+    return _INSTANCE

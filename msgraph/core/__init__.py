8cd221f02098d12c74428397d2289a0751c97261
samwelkanda <samwelkanda@gmail.com>
2022-11-01 21:09:52 +0300

    Remove duplicate files already in http core

--- msgraph/core/__init__.py
+++ msgraph/core/__init__.py
@@ -4,7 +4,6 @@
 # ------------------------------------
 from ._constants import SDK_VERSION
 from ._enums import APIVersion, NationalClouds
-from .graph_client import GraphClient
 from .graph_client_factory import GraphClientFactory
 
 __version__ = SDK_VERSION
--- msgraph/core/middleware/__init__.py
+++ msgraph/core/middleware/__init__.py
@@ -2,9 +2,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
-from .authorization import GraphAuthorizationHandler
-from .middleware import GraphMiddlewarePipeline, GraphRequest
-from .redirect import GraphRedirectHandler
 from .request_context import GraphRequestContext
-from .retry import GraphRetryHandler
 from .telemetry import GraphTelemetryHandler

ffb0495943926fc1b7292632f55b98ec9c02c956
samwelkanda <samwelkanda@gmail.com>
2022-11-01 21:08:30 +0300

    Update graph client factory and adapter

--- msgraph/core/graph_client_factory.py
+++ msgraph/core/graph_client_factory.py
@@ -7,18 +7,15 @@ from __future__ import annotations
 from typing import List, Optional
 
 import httpx
-from kiota_abstractions.authentication import AccessTokenProvider
 from kiota_http.kiota_client_factory import KiotaClientFactory
 from kiota_http.middleware import AsyncKiotaTransport
 from kiota_http.middleware.middleware import BaseMiddleware
 
-from .middleware import (
-    GraphAuthorizationHandler,
-    GraphMiddlewarePipeline,
-    GraphRedirectHandler,
-    GraphRetryHandler,
-    GraphTelemetryHandler,
-)
+from ._enums import APIVersion, NationalClouds
+from .middleware import GraphTelemetryHandler
+
+DEFAULT_CONNECTION_TIMEOUT: int = 30
+DEFAULT_REQUEST_TIMEOUT: int = 100
 
 
 class GraphClientFactory(KiotaClientFactory):
@@ -28,31 +25,37 @@ class GraphClientFactory(KiotaClientFactory):
 
     @staticmethod
     def create_with_default_middleware(
-        client: httpx.AsyncClient,
-        token_provider: Optional[AccessTokenProvider] = None
+        api_version: APIVersion = APIVersion.v1,
+        host: NationalClouds = NationalClouds.Global
     ) -> httpx.AsyncClient:
         """Constructs native HTTP AsyncClient(httpx.AsyncClient) instances configured with
         a custom transport loaded with a default pipeline of middleware.
         Returns:
             httpx.AsycClient: An instance of the AsyncClient object
         """
+        timeout = httpx.Timeout(DEFAULT_REQUEST_TIMEOUT, connect=DEFAULT_CONNECTION_TIMEOUT)
+        client = httpx.AsyncClient(
+            base_url=GraphClientFactory._get_base_url(host, api_version),
+            timeout=timeout,
+            http2=True
+        )
         current_transport = client._transport
-        middleware = GraphClientFactory._get_common_middleware()
-        if token_provider:
-            middleware.insert(0, GraphAuthorizationHandler(token_provider))
-
-        middleware_pipeline = GraphClientFactory._create_middleware_pipeline(
+        middleware = KiotaClientFactory._get_default_middleware()
+        middleware.append(GraphTelemetryHandler())
+        middleware_pipeline = KiotaClientFactory._create_middleware_pipeline(
             middleware, current_transport
         )
 
         client._transport = AsyncKiotaTransport(
-            transport=current_transport, middleware=middleware_pipeline
+            transport=current_transport, pipeline=middleware_pipeline
         )
         return client
 
     @staticmethod
     def create_with_custom_middleware(
-        client: httpx.AsyncClient, middleware: Optional[List[BaseMiddleware]]
+        middleware: Optional[List[BaseMiddleware]],
+        api_version: APIVersion = APIVersion.v1,
+        host: NationalClouds = NationalClouds.Global,
     ) -> httpx.AsyncClient:
         """Applies a custom middleware chain to the HTTP Client
 
@@ -61,34 +64,24 @@ class GraphClientFactory(KiotaClientFactory):
             a middleware pipeline. The middleware should be arranged in the order in which they will
             modify the request.
         """
+        timeout = httpx.Timeout(DEFAULT_REQUEST_TIMEOUT, connect=DEFAULT_CONNECTION_TIMEOUT)
+        client = httpx.AsyncClient(
+            base_url=GraphClientFactory._get_base_url(host, api_version),
+            timeout=timeout,
+            http2=True
+        )
         current_transport = client._transport
-        middleware_pipeline = GraphClientFactory._create_middleware_pipeline(
+        middleware_pipeline = KiotaClientFactory._create_middleware_pipeline(
             middleware, current_transport
         )
 
         client._transport = AsyncKiotaTransport(
-            transport=current_transport, middleware=middleware_pipeline
+            transport=current_transport, pipeline=middleware_pipeline
         )
         return client
 
     @staticmethod
-    def _get_common_middleware() -> List[BaseMiddleware]:
-        """
-        Helper method that returns a list of cross cutting middleware
-        """
-        middleware = [GraphRedirectHandler(), GraphRetryHandler(), GraphTelemetryHandler()]
-
-        return middleware
-
-    @staticmethod
-    def _create_middleware_pipeline(
-        middleware: Optional[List[BaseMiddleware]], transport: httpx.AsyncBaseTransport
-    ) -> GraphMiddlewarePipeline:
-        """
-        Helper method that constructs a middleware_pipeline with the specified middleware
-        """
-        middleware_pipeline = GraphMiddlewarePipeline(transport)
-        if middleware:
-            for ware in middleware:
-                middleware_pipeline.add_middleware(ware)
-        return middleware_pipeline
+    def _get_base_url(host: str, api_version: APIVersion) -> str:
+        """Helper method to set the complete base url"""
+        base_url = f'{host}/{api_version}'
+        return base_url

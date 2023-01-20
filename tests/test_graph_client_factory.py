2c337a9d693f77340399af7a1b1aaa992599cd44
samwelkanda <samwelkanda@gmail.com>
2023-01-20 17:55:05 +0300

    Update tests

--- tests/test_graph_client_factory.py
+++ tests/test_graph_client_factory.py
@@ -4,7 +4,8 @@
 # ------------------------------------
 import httpx
 import pytest
-from kiota_http.middleware import MiddlewarePipeline, RedirectHandler
+from kiota_http.middleware import MiddlewarePipeline, RedirectHandler, RetryHandler
+from kiota_http.middleware.options import RetryHandlerOption, RedirectHandlerOption
 
 from msgraph_core import APIVersion, GraphClientFactory, NationalClouds
 from msgraph_core.middleware import AsyncGraphTransport, GraphTelemetryHandler
@@ -19,6 +20,22 @@ def test_create_with_default_middleware():
     pipeline = client._transport.pipeline
     assert isinstance(pipeline, MiddlewarePipeline)
     assert isinstance(pipeline._first_middleware, RedirectHandler)
+    assert isinstance(pipeline._current_middleware, GraphTelemetryHandler)
+    
+def test_create_default_with_custom_middleware():
+    """Test creation of HTTP Client using default middleware and custom options"""
+    retry_options = RetryHandlerOption(max_retries=5)
+    options = {f'{retry_options.get_key()}': retry_options}
+    client = GraphClientFactory.create_with_default_middleware(options=options)
+
+    assert isinstance(client, httpx.AsyncClient)
+    assert isinstance(client._transport, AsyncGraphTransport)
+    pipeline = client._transport.pipeline
+    assert isinstance(pipeline, MiddlewarePipeline)
+    assert isinstance(pipeline._first_middleware, RedirectHandler)
+    retry_handler = pipeline._first_middleware.next
+    assert isinstance(retry_handler, RetryHandler)
+    assert retry_handler.max_retries == retry_options.max_retry
     assert isinstance(pipeline._current_middleware, GraphTelemetryHandler)
 
 

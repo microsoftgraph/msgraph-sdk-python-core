d213757cb718fe07558a92fa48b7245af59544f1
samwelkanda <samwelkanda@gmail.com>
2023-01-20 17:54:33 +0300

    Update graph client factory to accept options when constructing with default middleware

--- src/msgraph_core/graph_client_factory.py
+++ src/msgraph_core/graph_client_factory.py
@@ -4,14 +4,16 @@
 # ------------------------------------
 from __future__ import annotations
 
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 import httpx
+from kiota_abstractions.request_option import RequestOption
 from kiota_http.kiota_client_factory import KiotaClientFactory
 from kiota_http.middleware.middleware import BaseMiddleware
 
 from ._enums import APIVersion, NationalClouds
 from .middleware import AsyncGraphTransport, GraphTelemetryHandler
+from .middleware.options import GraphTelemetryHandlerOption
 
 
 class GraphClientFactory(KiotaClientFactory):
@@ -22,19 +24,30 @@ class GraphClientFactory(KiotaClientFactory):
     @staticmethod
     def create_with_default_middleware(
         api_version: APIVersion = APIVersion.v1,
-        host: NationalClouds = NationalClouds.Global
+        host: NationalClouds = NationalClouds.Global,
+        options: Optional[Dict[str, RequestOption]] = None
     ) -> httpx.AsyncClient:
         """Constructs native HTTP AsyncClient(httpx.AsyncClient) instances configured with
         a custom transport loaded with a default pipeline of middleware.
+
+        Args:
+            api_version (APIVersion): The Graph API version to be used.
+            Defaults to APIVersion.v1.
+            host (NationalClouds): The national clound endpoint to be used.
+            Defaults to NationalClouds.Global.
+            options (Optional[Dict[str, RequestOption]]): The request options to use when
+            instantiating default middleware. Defaults to Dict[str, RequestOption]=None.
+
         Returns:
-            httpx.AsycClient: An instance of the AsyncClient object
+            httpx.AsyncClient: An instance of the AsyncClient object
         """
         client = KiotaClientFactory.get_default_client()
         client.base_url = GraphClientFactory._get_base_url(host, api_version)
         current_transport = client._transport
 
-        middleware = KiotaClientFactory.get_default_middleware()
-        middleware.append(GraphTelemetryHandler())
+        middleware = KiotaClientFactory.get_default_middleware(options)
+        telemetry_handler = GraphClientFactory._get_telemetry_handler(options)
+        middleware.append(telemetry_handler)
         middleware_pipeline = KiotaClientFactory.create_middleware_pipeline(
             middleware, current_transport
         )
@@ -76,3 +89,16 @@ class GraphClientFactory(KiotaClientFactory):
         """Helper method to set the complete base url"""
         base_url = f'{host}/{api_version}'
         return base_url
+
+    @staticmethod
+    def _get_telemetry_handler(
+        options: Optional[Dict[str, RequestOption]]
+    ) -> GraphTelemetryHandler:
+        """Helper method to get the graph telemetry handler instantiated with appropriate
+        options"""
+
+        if options:
+            graph_telemetry_options = options.get(GraphTelemetryHandlerOption().get_key())
+            if graph_telemetry_options:
+                return GraphTelemetryHandler(options=graph_telemetry_options)
+        return GraphTelemetryHandler()

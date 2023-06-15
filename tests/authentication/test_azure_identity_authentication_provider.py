05ccf438a3db8ff0b1c76cc4aa8dcc2844c7b387
Martin Musale <martinmusale@microsoft.com>
2023-06-15 13:38:50 +0100

    Remove unnecessary test
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- tests/authentication/test_azure_identity_authentication_provider.py
+++ tests/authentication/test_azure_identity_authentication_provider.py
@@ -6,16 +6,3 @@ from msgraph_core.authentication import AzureIdentityAuthenticationProvider
 
 def test_subclassing():
     assert issubclass(AzureIdentityAuthenticationProvider, AuthenticationProvider)
-
-
-def test_create_provider():
-    credential = EnvironmentCredential()
-    auth_provider = AzureIdentityAuthenticationProvider(credential)
-
-    allowed_hosts = auth_provider.__dict__.get('allowed_hosts', None)
-    assert allowed_hosts is not None
-    assert 'https://graph.microsoft.com' in allowed_hosts
-
-    scopes = auth_provider.__dict__.get('scopes', None)
-    assert scopes is not None
-    assert 'https://graph.microsoft.com/.default' in scopes

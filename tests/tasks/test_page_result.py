3163da1888768561811f335b16d46ee91cfa2cd9
shemogumbe <shemogumbe@microsoft.com>
2024-02-01 10:45:41 +0300

    draft test for page_result model

---
+++ tests/tasks/test_page_result.py
@@ -0,0 +1,15 @@
+import pytest
+
+from kiota_serialization_json import JsonSerializationWriter
+
+from ..models import PageResult
+
+def test_set_and_get_values():
+    page_result = PageResult()
+    writer = JsonSerializationWriter()
+    writer.get_serialized_content(page_result)
+    page_result.set_value([{"name": "John Doe"}, {"name": "Ian Smith"}])
+    page_result.odata_next_link = "next_page"
+    assert 2 == len(page_result.value)
+    assert "next_page" == page_result.odata_next_link
+    assert {"@odata.nextLink": "next_page", "value": [{"name": "John Doe"}, {"name": "Ian Smith"}]} == writer.get_serialized_content(page_result)

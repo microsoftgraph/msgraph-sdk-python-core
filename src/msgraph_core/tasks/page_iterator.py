e3f46771143836c1bb1f323d5c40e022f240d6f9
shemogumbe <shemogumbe@microsoft.com>
2024-02-14 23:54:51 +0300

    Fix list assignment in page iterator

--- src/msgraph_core/tasks/page_iterator.py
+++ src/msgraph_core/tasks/page_iterator.py
@@ -75,7 +75,7 @@ class PageIterator:
             raise ValueError('Response cannot be null.')
         value = None
         if isinstance(response, list):
-            value: List[Any] = response.value
+            value = response.value
         elif hasattr(response, 'value'):
             value = getattr(response, 'value')
         elif isinstance(response, object):

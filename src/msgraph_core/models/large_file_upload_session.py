6ed2bfa8c4475ab62cf754270f03d5c4b7718bcc
shemogumbe <shemogumbe@microsoft.com>
2024-03-05 10:34:19 +0300

    Intitialize Large file upload models'

---
+++ src/msgraph_core/models/large_file_upload_session.py
@@ -0,0 +1,6 @@
+from kiota_abstractions.serialization.additional_data_holder import AdditionalDataHolder
+from kiota_abstractions.serialization.parsable import Parsable
+
+
+class LargeFileUploadSession(Parsable, AdditionalDataHolder):
+    pass

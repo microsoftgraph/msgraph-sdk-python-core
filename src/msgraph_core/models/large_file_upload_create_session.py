da8660f61e76dd6c46267179f27ab5e8e175adb5
shemogumbe <shemogumbe@microsoft.com>
2024-03-05 15:41:11 +0300

    Implement model classes for large file upload

--- src/msgraph_core/models/large_file_upload_create_session.py
+++ src/msgraph_core/models/large_file_upload_create_session.py
@@ -1,6 +1,27 @@
 from kiota_abstractions.serialization.additional_data_holder import AdditionalDataHolder
 from kiota_abstractions.serialization.parsable import Parsable
+from kiota_abstractions.store.backed_model import BackedModel
+from kiota_abstractions.store.backing_store import BackingStore
+from kiota_abstractions.store.backing_store_factory_singleton import BackingStoreFactorySingleton
 
 
-class LargeFileUploadCreateSession(Parsable, AdditionalDataHolder):
-    pass
+class LargeFileUploadCreateSessionBody(Parsable, AdditionalDataHolder):
+
+    def __init__(self):
+        self.backing_store = BackingStoreFactorySingleton.get_instance().create_backing_store()
+        self.set_additional_data([])
+
+    def get_additional_data(self):
+        return self.backing_store.get('additional_data')
+
+    def set_additional_data(self, value):
+        self.backing_store.set('additional_data', value)
+
+    def get_field_deserializers(self):
+        return {}
+
+    def serialize(self, writer):
+        writer.write_additional_data(self.get_additional_data())
+
+    def get_backing_store(self):
+        return self.backing_store

0f9883312819bcfd65790e2621ec8109e854b39b
shemogumbe <shemogumbe@microsoft.com>
2024-03-25 19:37:48 +0300

    Update Large file upload create session

--- src/msgraph_core/models/large_file_upload_create_session.py
+++ src/msgraph_core/models/large_file_upload_create_session.py
@@ -1,15 +1,17 @@
 from kiota_abstractions.serialization.additional_data_holder import AdditionalDataHolder
 from kiota_abstractions.serialization.parsable import Parsable
+from kiota_abstractions.serialization.serialization_writer import SerializationWriter
 from kiota_abstractions.store.backed_model import BackedModel
 from kiota_abstractions.store.backing_store import BackingStore
 from kiota_abstractions.store.backing_store_factory_singleton import BackingStoreFactorySingleton
 
 
-class LargeFileUploadCreateSessionBody(Parsable, AdditionalDataHolder):
+class LargeFileUploadCreateSessionBody(Parsable, AdditionalDataHolder, BackedModel):
 
     def __init__(self):
         self.backing_store = BackingStoreFactorySingleton.get_instance().create_backing_store()
         self.set_additional_data([])
+        self.backing_store = BackingStore()
 
     def get_additional_data(self):
         return self.backing_store.get('additional_data')
@@ -20,8 +22,8 @@ class LargeFileUploadCreateSessionBody(Parsable, AdditionalDataHolder):
     def get_field_deserializers(self):
         return {}
 
-    def serialize(self, writer):
-        writer.write_additional_data(self.get_additional_data())
+    def serialize(self, writer: SerializationWriter):
+        writer.write_additional_data_value(self.get_additional_data())
 
     def get_backing_store(self):
         return self.backing_store

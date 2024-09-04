91e9881588ddcd40bf0a79ac1f9a53b2b22f9282
shemogumbe <shemogumbe@microsoft.com>
2024-09-04 14:51:19 +0300

    add response collection base implementation

--- src/msgraph_core/requests/batch_response_content.py
+++ src/msgraph_core/requests/batch_response_content.py
@@ -1,4 +1,6 @@
-from typing import List, Optional, Dict, Callable
+from typing import Optional, Dict, Any, Type, TypeVar
+from io import BytesIO
+import base64
 
 from kiota_abstractions.serialization import Parsable
 from kiota_abstractions.serialization import ParseNode
@@ -8,27 +10,71 @@ from kiota_abstractions.serialization import SerializationWriter
 
 from .batch_response_item import BatchResponseItem
 
+T = TypeVar('T', bound='Parsable')
+
 
 class BatchResponseContent(Parsable):
-    responses = []
 
     def __init__(self) -> None:
-        pass
+        self._responses: Optional[Dict[str, 'BatchResponseItem']] = {}
 
     @property
-    def responses(self) -> List[Parsable]:
-        pass
+    def responses(self) -> Optional[Dict[str, 'BatchResponseItem']]:
+        return None if self._responses is None else self._responses
 
-    @property
-    def response(self, request_id: str) -> BatchResponseItem:
-        pass
+    @responses.setter
+    def responses(self, responses: Optional[Dict[str, 'BatchResponseItem']]) -> None:
+        if isinstance(responses, dict):
+            self._responses = {response.id: response for response in responses.values()}
+        else:
+            self._responses = responses
 
-    @property
-    def response_body(self, request_id: str) -> Optional[Parsable]:
-        pass
+    def response(self, request_id: str) -> 'BatchResponseItem':
+        if not self._responses or request_id not in self._responses:
+            raise ValueError(f"No response found for id: {request_id}")
+        return self._responses[request_id]
 
-    def get_field_deserializers(self, ) -> Dict[str, Callable[[ParseNode], None]]:
-        pass
+    def response_body(self, request_id: str, type: Type[T]) -> Optional[T]:
+        if not self._responses or request_id not in self._responses:
+            raise ValueError(f"No response found for id: {request_id}")
+
+        if not issubclass(type, Parsable):
+            raise ValueError("Type passed must implement the Parsable interface")
+
+        response = self._responses[request_id]
+        content_type = response.content_type
+        if not content_type:
+            raise RuntimeError("Unable to get content-type header in response item")
+
+        response_body = response.body or BytesIO()
+        try:
+            try:
+                parse_node = ParseNodeFactoryRegistry().get_root_parse_node(
+                    content_type, response_body
+                )
+            except Exception:
+                response_body.seek(0)
+                base64_decoded_body = BytesIO(base64.b64decode(response_body.read()))
+                parse_node = ParseNodeFactoryRegistry().get_root_parse_node(
+                    content_type, base64_decoded_body
+                )
+                response.body = base64_decoded_body
+            return parse_node.get_object_value(type.create_from_discriminator_value)
+            # tests this
+        except Exception:
+            raise ValueError(
+                f"Unable to deserialize batch response for request Id: {request_id} to {type}"
+            )
+
+    def get_field_deserializers(self) -> Dict[str, Any]:
+        return {
+            'responses':
+            lambda n: self.responses(n.get_collection_of_object_values(BatchResponseItem.create))
+        }
 
     def serialize(self, writer: SerializationWriter) -> None:
-        pass
+        writer.write_collection_of_object_values('responses', self._responses.values())
+
+    @staticmethod
+    def create_from_discriminator_value(parse_node: ParseNode) -> 'BatchResponseContent':
+        return BatchResponseContent()
--- src/msgraph_core/requests/batch_response_content_collection.py
+++ src/msgraph_core/requests/batch_response_content_collection.py
@@ -7,15 +7,39 @@ from kiota_abstractions.serialization import ParseNodeFactoryRegistry
 from kiota_abstractions.serialization import SerializationWriter
 
 from .batch_response_content import BatchResponseContent
+from .batch_response_item import BatchResponseItem
 
 
 class BatchResponseContentCollection(Parsable):
 
     def __init__(self) -> None:
-        pass
+        self._responses: BatchResponseContent = BatchResponseContent()
 
-    def get_response(self, request_id: str) -> BatchResponseContent:
-        pass
+    def add_response(self, keys: List[str], content: BatchResponseContent):
+        for key in keys:
+            self._responses.append({key: content})  # fix this to BatchResponseItem
 
-    def get_response_status_codes():
-        pass
+    async def get_response_by_id(self, request_id: str) -> Optional[BatchResponseItem]:
+        """
+        Get a response by its request ID from the collection        
+        :param request_id: The request ID of the response to get
+        :type request_id: str
+        :return: The response with the specified request ID as a BatchResponseItem
+        :rtype: Optional[BatchResponseItem]
+        """
+        if not self._responses:
+            raise ValueError("No responses found in the collection")
+        if (isinstance(self._responses, BatchResponseContent)):
+            for response in self._responses:
+                if request_id in response:
+                    return self._responses.response(request_id)
+        raise TypeError("Invalid type: Collection must be of type BatchResponseContent")
+
+    @property
+    async def responses_status_codes(self) -> Dict[str, int]:
+        status_codes: Dict[str, int] = {}
+        for response in self._responses:
+            if (isinstance(response, BatchResponseItem)):
+                status_codes[response.id] = response.status_code
+            raise TypeError("Invalid type: Collection must be of type BatchResponseContent")
+        return status_codes

8376c4ce1a686b7a25cdba0016d6654ba523e6fa
shemogumbe <shemogumbe@microsoft.com>
2024-09-06 18:07:08 +0300

    fix request content collection creation

--- src/msgraph_core/requests/batch_request_content_collection.py
+++ src/msgraph_core/requests/batch_request_content_collection.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from kiota_abstractions.request_information import RequestInformation
 
@@ -9,13 +9,12 @@ from .batch_request_item import BatchRequestItem
 class BatchRequestContentCollection:
     """A collection of request content objects."""
 
-    def __init__(self, batch_max_requests: int = 20):
-        self.batch_max_requests = batch_max_requests or BatchRequestContent.MAX_REQUESTS
-        self.batches: List[BatchRequestContent] = []
-        self.current_batch: BatchRequestContent = BatchRequestContent(
-        )  # fix, how we get the current batch?
+    def __init__(self, batch_request_limit: int = 20):
+        self.batch_request_limit = batch_request_limit or BatchRequestContent.MAX_REQUESTS
+        self.batches: [List[BatchRequestContent]] = []
+        self.current_batch: BatchRequestContent = BatchRequestContent()
 
-    def add_batch_request_step(self, request: BatchRequestItem) -> None:
+    def add_batch_request_item(self, request: BatchRequestItem) -> None:
         try:
             self.current_batch.add_request(request)
         except ValueError as e:
@@ -24,10 +23,30 @@ class BatchRequestContentCollection:
 
                 self.current_batch = BatchRequestContent()
                 self.current_batch.add_request(request)
-            else:
-                raise e
+        self.batches.append(self.current_batch)
 
-    def get_batch_requests_for_execution(self):
+    def remove_batch_request_item(self, request_id: str) -> None:
+        for batch in self.batches:
+            for request in batch.requests:
+                if request.id == request_id:
+                    batch.requests.remove(request)
+                    return
+        for request in self.current_batch.requests:
+            if request.id == request_id:
+                self.current_batch.requests.remove(request)
+                return
+
+    def new_batch_with_failed_requests(self) -> Optional[BatchRequestContent]:
+        # Use IDs to get response status codes, generate new batch with failed requests
+        batch_with_failed_responses: Optional[BatchRequestContent] = BatchRequestContent()
+        for batch in self.batches:
+            for request in batch.requests:
+                if request.status_code != 200:
+                    return batch_with_failed_responses.add_request(request)
+        return batch_with_failed_responses
+
+    def get_batch_requests_for_execution(self) -> List[BatchRequestContent]:
         if not self.current_batch.is_finalized:
-            self.batches.append(self.current_batch.finalize())
+            self.current_batch.finalize()
+            self.batches.append(self.current_batch)
         return self.batches

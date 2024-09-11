8b988db0d64a36505870c47b74d22f06ca82436d
shemogumbe <shemogumbe@microsoft.com>
2024-09-11 12:58:06 +0300

    add samples for batch request with collection body sample

---
+++ samples/batch_requests/batch_request_with_collection.py
@@ -0,0 +1,106 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+#pylint: disable=undefined-variable
+"""Demonstrates using the Batch request with Collection"""
+import asyncio
+
+from urllib.request import Request
+from kiota_abstractions.request_information import RequestInformation
+
+from msgraph import GraphServiceClient
+
+from kiota_abstractions.headers_collection import HeadersCollection as RequestHeaders
+from msgraph_core.requests.batch_request_item import BatchRequestItem
+
+from msgraph_core.requests.batch_request_content import BatchRequestContent
+from msgraph_core.requests.batch_request_content_collection import BatchRequestContentCollection
+from msgraph_core.requests.batch_request_builder import BatchRequestBuilder
+# Create a client
+# code to create graph client
+
+user_client = GraphServiceClient(credentials=token, scopes=graph_scopes)
+
+# Create a request adapter from the client
+request_adapter = user_client.request_adapter
+
+# Create an instance of BatchRequestBuilder
+batch_request_builder = BatchRequestBuilder(request_adapter)
+
+# Create some BatchRequestItems
+
+request_info1 = RequestInformation()
+request_info1.http_method = "GET"
+request_info1.url = "/me"
+request_info1.headers = RequestHeaders()
+request_info1.headers.add("Content-Type", "application/json")
+
+request_info2 = RequestInformation()
+request_info2.http_method = "GET"
+request_info2.url = "/users"
+request_info2.headers = RequestHeaders()
+request_info2.headers.add("Content-Type", "application/json")
+
+request_info3 = RequestInformation()
+request_info3.http_method = "GET"
+request_info3.url = "/me"
+request_info3.headers = RequestHeaders()
+request_info3.headers.add("Content-Type", "application/json")
+
+# Create BatchRequestItem instances
+batch_request_item1 = BatchRequestItem(request_information=request_info1)
+batch_request_item2 = BatchRequestItem(request_information=request_info2)
+
+# Create batch request content
+# Create a BatchRequestContent instance and add requests
+batch_request_content_items = [batch_request_item1, batch_request_item2]
+batch_request_content = BatchRequestContent(requests=batch_request_content_items)
+batch_request_content.add_request(batch_request_item1)
+batch_request_content.add_request(batch_request_item2)
+
+# Add a request using RequestInformation directly
+batch_request_content.add_request_information(request_info1)
+print(
+    f"Number of requests after adding request using RequestInformation: {len(batch_request_content.requests)}"
+)
+print("------------------------------------------------------------------------------------")
+# Create an instance of BatchRequestContentCollection
+collection = BatchRequestContentCollection()
+# Add request items to the collection
+batch_request_item_to_add = BatchRequestItem(request_information=request_info3)
+batch_request_item_to_add1 = BatchRequestItem(request_information=request_info3)
+batch_request_item_to_add2 = BatchRequestItem(request_information=request_info3)
+batch_request_item_to_add3 = BatchRequestItem(request_information=request_info3)
+batch_request_item_to_add4 = BatchRequestItem(request_information=request_info3)
+
+collection.add_batch_request_item(batch_request_item_to_add)
+collection.add_batch_request_item(batch_request_item_to_add1)
+collection.add_batch_request_item(batch_request_item_to_add2)
+collection.add_batch_request_item(batch_request_item_to_add3)
+collection.add_batch_request_item(batch_request_item_to_add4)
+
+# Print the current batch requests
+print("Current Batch Requests:")
+for request in collection.current_batch.requests:
+    print(f"Request ID: {request.id}, Status Code: {request.headers}")
+
+# Remove a request item from the collection
+collection.remove_batch_request_item(batch_request_item_to_add.id)
+print(f"Items left in the batch after removal: {len(collection.current_batch.requests)}")
+
+
+# post a collection
+async def main():
+
+    batch_response_content = await batch_request_builder.post(batch_request_content=collection)
+    responses = batch_response_content.get_responses()
+    for item in responses:
+        for item_body in item.responses:
+            print(f"Item: {item_body.id}, Status Code: {item_body.status}")
+            print(f"body: {item_body.body} Item headers: {item_body.headers} ")
+            print("-----------------------------------------------")
+
+
+# Run the main function
+asyncio.run(main())
